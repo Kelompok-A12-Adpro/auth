e580721750826d236933cd94094fd2b25f50b321
Valentino <valentino.kim@ui.ac.id>
2025-04-09 22:00:18 +0700

    feat: added routes

---
+++ src/routes.rs
@@ -0,0 +1,7 @@
+use actix_web::web;
+use crate::controllers::auth_controller::{register_user_controller, login_user_controller};
+
+pub fn init_routes(cfg: &mut web::ServiceConfig) {
+    cfg.route("/register", web::post().to(register_user_controller))
+        .route("/login", web::post().to(login_user_controller));
+}
