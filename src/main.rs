12b37ac530d33091dfaa0eb9725267de314ee58d
Valentino <valentino.kim@ui.ac.id>
2025-04-09 22:02:08 +0700

    feat: added main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
+use std::env;
+
+mod controller;
+mod service;
+mod repository;
+mod models;
+mod schema;
+mod routes;
+
+type PoolType = Pool<ConnectionManager<PgConnection>>;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .configure(routes::init_routes) // Configure the routes
+    })
+    .bind(format!("{}:{}", host, port))?
+    .run()
+    .await
 }
