34ca745fe0b8aaefa91c9e459e9aa6ef488b6352
Valentino <valentino.kim@ui.ac.id>
2025-04-09 20:36:24 +0700

    chore: init rust project, added dependencies

---
+++ target/debug/deps/auth-02b092cbc22223ae.d
@@ -0,0 +1,5 @@
+C:\Users\Valen\Documents\TUGAS BRAY\Semester 4\ADPRO\TK\auth\target\debug\deps\libauth-02b092cbc22223ae.rmeta: src\main.rs
+
+C:\Users\Valen\Documents\TUGAS BRAY\Semester 4\ADPRO\TK\auth\target\debug\deps\auth-02b092cbc22223ae.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/auth-ef5d2719672ca872.d
@@ -0,0 +1,5 @@
+C:\Users\Valen\Documents\TUGAS BRAY\Semester 4\ADPRO\TK\auth\target\debug\deps\libauth-ef5d2719672ca872.rmeta: src\main.rs
+
+C:\Users\Valen\Documents\TUGAS BRAY\Semester 4\ADPRO\TK\auth\target\debug\deps\auth-ef5d2719672ca872.d: src\main.rs
+
+src\main.rs:
