ec149ca6791e04c3bed22d32504b5780acaf7bd8
Valentino <valentino.kim@ui.ac.id>
2025-04-09 21:13:53 +0700

    feat: added database for users, add gitignore

---
+++ migrations/2025-04-09-141255_create_users/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL
+);
---
+++ src/schema.rs
@@ -0,0 +1,11 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        email -> Varchar,
+        password -> Varchar,
+        name -> Varchar,
+        phone -> Varchar,
+    }
+}
