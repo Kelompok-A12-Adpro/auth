bf53df5a998192ececb0367fbd866522524e3492
Valentino <valentino.kim@ui.ac.id>
2025-04-11 20:25:35 +0700

    feat: update module imports and add routes module

--- src/routes/routes.rs
+++ src/routes/routes.rs
@@ -1,5 +1,5 @@
 use actix_web::web;
-use crate::controller::{register_user_controller, login_user_controller};
+use crate::controller::user_controller::{register_user_controller, login_user_controller};
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/register", web::post().to(register_user_controller))
